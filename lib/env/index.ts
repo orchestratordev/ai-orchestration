79c599102ce5b22b947457827f1c6d1f6f7bedaf
orchestratordev <dmip.dokb@gmail.com>
2026-06-19 11:25:16 +0800

    Add environment variables configuration

---
+++ lib/env/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Environment Variables
+ */
+
+export const env = {
+  NODE_ENV: process.env.NODE_ENV,
+
+  APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+
+  APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+
+  SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+
+  SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+};
