b4ad2a4e23ed35111cdb935dbe97086333c7631f
orchestratordev <dmip.dokb@gmail.com>
2026-06-20 06:42:30 +0800

    Set default NODE_ENV to 'development'

--- lib/env/server.ts
+++ lib/env/server.ts
@@ -1,9 +1,5 @@
-/**
- * Server Environment
- */
-
 export const serverEnv = {
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV ?? 'development',
 
   SUPABASE_SERVICE_ROLE_KEY:
     process.env.SUPABASE_SERVICE_ROLE_KEY,
